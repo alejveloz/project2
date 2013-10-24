d695708ecde06dc43862d759fb9db23dda937a13
Alejandro Veloz <aveloz.intern@beatsbydre.com>
2013-10-23 22:14:18 -0700

    Added psuedo code to MyParser.java. Removed from pseudo.txt.

--- src/edu/ucla/cs/cs144/MyParser.java
+++ src/edu/ucla/cs/cs144/MyParser.java
@@ -28,6 +28,7 @@ package edu.ucla.cs.cs144;
 import java.io.*;
 import java.text.*;
 import java.util.*;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.FactoryConfigurationError;
@@ -61,6 +62,12 @@ class MyParser {
 	"DocFragment",
 	"Notation",
     };
+    
+    LinkedHashMap<Integer, Item> itemMap;
+    LinkedHashMap<Integer, User> userMap;
+    LinkedHashMap<Integer, String> categoryIDMap;
+    LinkedHashMap<String, Integer> categoryStringMap;
+    ArrayList<Bid> bids;
     
     static class MyErrorHandler implements ErrorHandler {
         
@@ -179,14 +186,132 @@ class MyParser {
         /* At this point 'doc' contains a DOM representation of an 'Items' XML
          * file. Use doc.getDocumentElement() to get the root Element. */
         System.out.println("Successfully parsed - " + xmlFile);
+
         
-        /* Fill in code here (you will probably need to write auxiliary
-            methods). */
-        
-        
+        // Get all Item elements
+		// Process each element
+			// Create an Item by parsing the the "Item" element
+			parseItem();
+			// Add the resulting item to items[]
+			addItem();
         
         /**************************************************************/
         
+    }
+    
+  
+    static void parseItem()
+    {
+		// Build most of Item object
+		// Create a new Item object
+		// Grab the "itemID" and set it
+		// Grab the "name" and set it
+    	// Grab the "buyPrice" and set it
+    	// Grab the "firstMinimumBid" and set it
+		// Grab the "numBids" and set it
+    	// Grab the "currentBidAmount" and set it
+    	// Grab the "started" and set it
+    	// Grab the "ends" and set it
+    	// Grab the "description" and set it
+			
+		// Build User object for seller	
+    	// Create a User by parsing the "Seller" element
+    	parseSeller();
+    	// Grab Location and set the user's "location"
+    	// Grab Country and set the user's "country"
+    	// Add the user and account for conflicts
+    	addUser();
+		
+		// Build Categories array
+    	// Create an array of category strings by parsing the "Category"s
+    	// Get an array of the equivalent category ID's
+		getCategoryIDs();
+		
+		// Process Bids
+    	// Hand off the bids to be parsed for Bid and User info
+    	parseBids();
+    	// For each big, update it's ItemID
+		
+		// Set the remaining fields of the Item object
+    	// Grab the User's ID and set item's "userID"
+    	// Set the item's category array
+    }
+    
+    static void parseSeller()
+    {
+    	// Create a User object
+    	// Grab the "userID" and set it
+    	// Grab the "rating" and set it
+ 
+    	// Return the User object
+    }
+    
+    static void parseBids()
+    {
+    	// For each bid element, process
+    	// Parse the bid for User info and Bid info
+    	parseBid();
+    	// Add the resulting Bid to the bids[] array
+    }
+    
+    static void parseBid()
+    {
+    	// Build most of the Bid object
+    	// Grab the "time" and set it
+    	// Grab the "amount" and set it
+    	
+    	// Process Bidder object for User
+    	// Create a user from the Bidder element
+    	parseBidder();
+    	// Add the user and account for conflicts
+    	addUser();
+    			
+    	// Set the remaining fields of the Bid object
+    	// Set the Bid item's "userID"
+    }
+    
+    static void parseBidder()
+    {
+    	// Create a User object
+    	// Grab the "userID" and set it
+    	// Grab the "rating" and set it
+    }
+    
+    static void getCategoryIDs()
+    {
+    	// Create a return array
+    	// Process each Category string
+    		// Check for category in string->id map
+    		// If it exists
+    			// Add it to the return array
+    		// If it doesn't
+    			// Create a new id (increment) 
+    			// Add  pair to string->id, id->string maps
+    			// Add new id to return array
+    	// Return the array
+    }
+    
+    static void addItem()
+    {
+    	// If the item doesn't exist in itemMap
+    	// Add it
+    }
+    
+    static void addUser()
+    {
+    	// If the user doesn't exist in userMap
+    		// Add them
+    	// If the user does exist in userMap
+    		// Grab the existing User
+    		// Merge the two Users to create a new user
+    		mergeUsers();
+    }
+    
+    static void mergeUsers()
+    {
+    	// Compare field by field
+    	// Update first User to gain any missing attributes
+    	// Print any conflicts (should not occur)
     }
     
     public static void main (String[] args) {
