27c01825eaecc71b67cc5c2b8f5a24a538fd1114
Alejandro Veloz <aveloz.intern@beatsbydre.com>
2013-10-25 10:43:09 -0700

    Added query comments

--- queries.sql
+++ queries.sql
@@ -1,1 +1,13 @@
-SELECT name FROM Actors WHERE movie = 'Die Another Day';
+-- Find the number of users in the database.
+
+-- Find the number of sellers from "New York", (i.e., sellers whose location is exactly the string "New York"). Pay special attention to case sensitivity. You should match the sellers from "New York" but not from "new york".
+
+-- Find the number of auctions belonging to exactly four categories.
+
+-- Find the ID(s) of current (unsold) auction(s) with the highest bid. Remember that the data was captured at the point in time December 20th, 2001, one second after midnight, so you can use this time point to decide which auction(s) are current. Pay special attention to the current auctions without any bid.
+
+-- Find the number of sellers whose rating is higher than 1000.
+
+-- Find the number of users who are both sellers and bidders.
+
+-- Find the number of categories that include at least one item with a bid of more than $100.
