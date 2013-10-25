92747ef5358a92ae0febd4184515754319ff8a40
Alejandro Veloz <aveloz.intern@beatsbydre.com>
2013-10-25 14:54:59 -0700

    Added 1 new query, fixed old query

--- queries.sql
+++ queries.sql
@@ -3,11 +3,11 @@ select count(*) from User;
 -- 13422 as expected
 
 -- Find the number of sellers from "New York", (i.e., sellers whose location is exactly the string "New York"). Pay special attention to case sensitivity. You should match the sellers from "New York" but not from "new york".
-select count(*) 
+select count(distinct User.id) 
 from User, ItemSeller 
 where User.id like ItemSeller.uid 
 and binary User.location like "New York";
--- 103 instead 80
+-- 80 as expected
 
 -- Find the number of auctions belonging to exactly four categories.
 -- Example GROUPBY: http://www.w3schools.com/sql/trysql.asp?filename=trysql_select_groupby
@@ -24,6 +24,7 @@ where CategoryCount.NumberOfCategories=4;
 -- Find the ID(s) of current (unsold) auction(s) with the highest bid. Remember that the data was captured at the point in time December 20th, 2001, one second after midnight, so you can use this time point to decide which auction(s) are current. Pay special attention to the current auctions without any bid.
 
 -- Find the number of sellers whose rating is higher than 1000.
+select count(distinct User.id) from User, ItemSeller where User.id like ItemSeller.uid and User.rating > 1000;
 
 -- Find the number of users who are both sellers and bidders.
 
