2eb77d4f523e252f2dc39abcba77d964fccf0b60
Alejandro Veloz <aveloz.intern@beatsbydre.com>
2013-10-25 00:48:00 -0700

    Updated skeleton

--- runLoad.sh
+++ runLoad.sh
@@ -10,15 +10,9 @@ mysql CS144 < create.sql
 # Compile and run the parser to generate the appropriate load files
 ant
 ant run-all
-...
-
-# If the Java code does not handle duplicate removal, do this now
-sort ...
-...
 
 # Run the load.sql batch file to load the data
 mysql CS144 < load.sql
 
 # Remove all temporary files
-rm ...
-...
+# rm
