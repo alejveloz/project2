0d01a781a67a530cb601cb979b46c4f513b34aa7
Alejandro Veloz <aveloz.intern@beatsbydre.com>
2013-10-25 10:33:27 -0700

    Added mysql table creation commands

--- create.sql
+++ create.sql
@@ -1,64 +1,39 @@
--- A movie's ID should be unique, must have a title, and a positive id
-CREATE TABLE Movie(id int PRIMARY KEY, 
-                   title varchar(100) NOT NULL, 
-                   year int, 
-                   rating varchar(10), 
-                   company varchar(50),
-                   CHECK(id>0)) 
-                   ENGINE=INNODB;
+CREATE TABLE Item(id int PRIMARY KEY, 
+                  name varchar(100) NOT NULL,
+                  buy_price double,
+                  first_bid double NOT NULL,
+                  started date NOT NULL,
+                  ends date NOT NULL,
+                  description varchar(4000) NOT NULL,
+                  number_of_bids int NOT NULL,
+                  currently double NOT_NULL)
+                  ENGINE=INNODB;
 
--- An actor's ID should be unique, must have first/last name, must have dob, dod must be after dob, and a positive id
-CREATE TABLE Actor(id int PRIMARY KEY, 
-                   last varchar(20) NOT NULL, 
-                   first varchar(20) NOT NULL, 
-                   sex varchar(6), 
-                   dob date NOT NULL, 
-                   dod date,
-                   CHECK((dod = "" OR (dod != "" AND dod >= dob) AND id > 0)))
-                   ENGINE=INNODB;
+CREATE TABLE User(id varchar(50) PRIMARY KEY,
+                  rating int NOT NULL,
+                  location varchar(50),
+                  country varchar(50)
+                  ENGINE=INNODB;
 
--- A director's ID should be unique, must have first/last name, must have dob, dod must be after dob, and a positive id
-CREATE TABLE Director(id int PRIMARY KEY,
-                      last varchar(20) NOT NULL,
-                      first varchar(20) NOT NULL, 
-                      dob date NOT NULL, 
-                      dod date,
-                      CHECK((dod = "" OR (dod != "" AND dod >= dob) AND id > 0)))
-                      ENGINE=INNODB;
+CREATE TABLE Category(name varchar(30))ENGINE=INNODB;
 
--- A MovieGenre relation must reference a valid mid in Movie
-CREATE TABLE MovieGenre(mid int, 
-                        genre varchar(20),
-                        FOREIGN KEY(mid) references Movie(id))
+CREATE TABLE Bid(iid int,
+                 time date,
+                 uid varchar(50),
+                 amount double,
+                 PRIMARY KEY (iid, time)
+                 FOREIGN KEY(iid) references Item(id),
+                 FOREIGN KEY(uid) references User(id))
+                 ENGINE=INNODB;
+
+CREATE TABLE ItemSeller(iid int PRIMARY KEY,
+                        uid varchar(5),
+                        FOREIGN KEY(iid) references Item(id),
+                        FOREIGN KEY(uid) references User(id))
                         ENGINE=INNODB;
 
--- A MovieDirector relation must reference a valid mid in Movie and did in Director
-CREATE TABLE MovieDirector(mid int, 
-                           did int,
-                           FOREIGN KEY(mid) references Movie(id),
-                           FOREIGN KEY(did) references Director(id))
-                           ENGINE=INNODB;
-
--- A MovieActor relation must reference a valid mid in Movie and aid in Actor
-CREATE TABLE MovieActor(mid int, 
-                        aid int, 
-                        role varchar(50),
-                        FOREIGN KEY(mid) references Movie(id),
-                        FOREIGN KEY(aid) references Actor(id))
-                        ENGINE=INNODB;
-
--- A Review relation must reference a valid mid in Movie, rating must be between 0 and 5
-CREATE TABLE Review(name varchar(20), 
-                    time timestamp, 
-                    mid int, 
-                    rating int, 
-                    comment varchar(500),
-                    FOREIGN KEY(mid) references Movie(id),
-                    CHECK(rating>=0 AND rating<=5))
-                    ENGINE=INNODB;
-
--- A MaxPersonID id must never be null
-CREATE TABLE MaxPersonID(id int NOT NULL) ENGINE=INNODB;
-
---A MaxMovieID id must never be null
-CREATE TABLE MaxMovieID(id int NOT NULL) ENGINE=INNODB;
+CREATE TABLE ItemCategory(iid int,
+                          category varchar(30),
+                          FOREIGN KEY(iid) references Item(id),
+                          FOREIGN KEY(category references Category(name))
+                          ENGINE=INNODB;
